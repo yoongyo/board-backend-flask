dc6c1d55191ac87d57b2c3eafaab806e8f224218
yoongyo <jyg0172@naver.com>
2021-08-01 18:30:05 +0900

    db

--- config/production.py
+++ config/production.py
@@ -1,5 +1,7 @@
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'board.db'))
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
